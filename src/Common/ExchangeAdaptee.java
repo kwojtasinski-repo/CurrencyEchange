5d7ecf07bee713ab84f8b707aa19e5521272cb3e
Wojtasiński Kamil <kamil.wojtasinski@streamsoft.pl>
2021-02-01 13:22:21 +0100

    Changes in main

--- src/Common/ExchangeAdaptee.java
+++ src/Common/ExchangeAdaptee.java
@@ -56,7 +56,7 @@ public class ExchangeAdaptee
 	    catch (JSONException e) 
 	    {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 	    }
 
 		return rate;
